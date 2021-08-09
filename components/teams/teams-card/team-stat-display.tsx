d5abc12f9dcebed24d485ace6f7abefe2293958b
Karel Koudelka <k.koudelka01@gmail.com>
2021-08-09 02:25:48 +0200

    feat: add team points display

--- components/teams/teams-card/team-stat-display.tsx
+++ components/teams/teams-card/team-stat-display.tsx
@@ -1,12 +1,14 @@
 import { useQuery } from "@apollo/client";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Tooltip from "@material-ui/core/Tooltip";
+import Typography from "@material-ui/core/Typography";
 import React from "react";
 import { getTeamPointSummaryQuery } from "../../../src/gql/client/queries";
 import {
   GetTeamSummary,
   GetTeamSummaryVariables,
 } from "../../../src/gql/client/types/GetTeamSummary";
+import { totalPoints } from "../../../src/utils/point-ratio";
 
 interface IProps {
   teamId: string;
@@ -35,35 +37,40 @@ const TeamStatDisplay: React.FC<IProps> = ({ teamId }) => {
     return null;
 
   return (
-    <div className="progress" style={{ height: 7.5 }}>
-      <Tooltip title={`Mining - ${summary.mining} bodů`}>
-        <div
-          className="progress-bar bkg-mining"
-          role="progressbar"
-          style={{ width: `${ratios.mining * 100}%` }}
-        ></div>
-      </Tooltip>
-      <Tooltip title={`Crafting - ${summary.crafting} bodů`}>
-        <div
-          className="progress-bar bkg-crafting"
-          role="progressbar"
-          style={{ width: `${ratios.crafting * 100}%` }}
-        ></div>
-      </Tooltip>
-      <Tooltip title={`Warfare - ${summary.warfare} bodů`}>
-        <div
-          className="progress-bar bkg-warfare"
-          role="progressbar"
-          style={{ width: `${ratios.warfare * 100}%` }}
-        ></div>
-      </Tooltip>
-      <Tooltip title={`Journey - ${summary.journey} bodů`}>
-        <div
-          className="progress-bar bkg-journey"
-          role="progressbar"
-          style={{ width: `${ratios.journey * 100}%` }}
-        ></div>
-      </Tooltip>
+    <div className="d-flex flex-column">
+      <div className="progress w-100" style={{ height: 7.5 }}>
+        <Tooltip title={`Mining - ${summary.mining} bodů`}>
+          <div
+            className="progress-bar bkg-mining"
+            role="progressbar"
+            style={{ width: `${ratios.mining * 100}%` }}
+          ></div>
+        </Tooltip>
+        <Tooltip title={`Crafting - ${summary.crafting} bodů`}>
+          <div
+            className="progress-bar bkg-crafting"
+            role="progressbar"
+            style={{ width: `${ratios.crafting * 100}%` }}
+          ></div>
+        </Tooltip>
+        <Tooltip title={`Warfare - ${summary.warfare} bodů`}>
+          <div
+            className="progress-bar bkg-warfare"
+            role="progressbar"
+            style={{ width: `${ratios.warfare * 100}%` }}
+          ></div>
+        </Tooltip>
+        <Tooltip title={`Journey - ${summary.journey} bodů`}>
+          <div
+            className="progress-bar bkg-journey"
+            role="progressbar"
+            style={{ width: `${ratios.journey * 100}%` }}
+          ></div>
+        </Tooltip>
+      </div>
+      <div className="my-2">
+        <Typography>{totalPoints(summary)} bodů</Typography>
+      </div>
     </div>
   );
 };
